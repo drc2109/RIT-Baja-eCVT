d9748f67ed4418b04993fd4ecd249ba5c2188f8b
Josh Ellsworth <jellsworth3663@gmail.com>
2026-04-25 19:04:57 -0400

    pushing working on car changes

---
+++ msd_baja_ecvt_ecu/Debug/Drivers/adrian_controller_old/Controller_P7_data.d
@@ -0,0 +1,28 @@
+Drivers/adrian_controller_old/Controller_P7_data.o: \
+ ../Drivers/adrian_controller_old/Controller_P7_data.c \
+ ../Drivers/adrian_controller_old/Controller_P7.h \
+ ../Drivers/adrian_controller_old/rtwtypes.h \
+ ../Drivers/adrian_controller_old/tmwtypes.h \
+ ../Drivers/adrian_controller_old/rtw_continuous.h \
+ ../Drivers/adrian_controller_old/rtw_solver.h \
+ ../Drivers/adrian_controller_old/rt_logging.h \
+ ../Drivers/adrian_controller_old/builtin_typeid_types.h \
+ ../Drivers/adrian_controller_old/multiword_types.h \
+ ../Drivers/adrian_controller_old/rt_mxclassid.h \
+ ../Drivers/adrian_controller_old/rtw_matlogging.h \
+ ../Drivers/adrian_controller_old/sl_types_def.h \
+ ../Drivers/adrian_controller_old/rt_nonfinite.h \
+ ../Drivers/adrian_controller_old/Controller_P7_types.h
+../Drivers/adrian_controller_old/Controller_P7.h:
+../Drivers/adrian_controller_old/rtwtypes.h:
+../Drivers/adrian_controller_old/tmwtypes.h:
+../Drivers/adrian_controller_old/rtw_continuous.h:
+../Drivers/adrian_controller_old/rtw_solver.h:
+../Drivers/adrian_controller_old/rt_logging.h:
+../Drivers/adrian_controller_old/builtin_typeid_types.h:
+../Drivers/adrian_controller_old/multiword_types.h:
+../Drivers/adrian_controller_old/rt_mxclassid.h:
+../Drivers/adrian_controller_old/rtw_matlogging.h:
+../Drivers/adrian_controller_old/sl_types_def.h:
+../Drivers/adrian_controller_old/rt_nonfinite.h:
+../Drivers/adrian_controller_old/Controller_P7_types.h:
---
+++ msd_baja_ecvt_ecu/Debug/Drivers/adrian_controller_old/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (14.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/adrian_controller_old/Controller_P7.c \
+../Drivers/adrian_controller_old/Controller_P7_data.c \
+../Drivers/adrian_controller_old/rt_logging.c \
+../Drivers/adrian_controller_old/rt_nonfinite.c \
+../Drivers/adrian_controller_old/testmain.c 
+
+OBJS += \
+./Drivers/adrian_controller_old/Controller_P7.o \
+./Drivers/adrian_controller_old/Controller_P7_data.o \
+./Drivers/adrian_controller_old/rt_logging.o \
+./Drivers/adrian_controller_old/rt_nonfinite.o \
+./Drivers/adrian_controller_old/testmain.o 
+
+C_DEPS += \
+./Drivers/adrian_controller_old/Controller_P7.d \
+./Drivers/adrian_controller_old/Controller_P7_data.d \
+./Drivers/adrian_controller_old/rt_logging.d \
+./Drivers/adrian_controller_old/rt_nonfinite.d \
+./Drivers/adrian_controller_old/testmain.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/adrian_controller_old/%.o Drivers/adrian_controller_old/%.su Drivers/adrian_controller_old/%.cyclo: ../Drivers/adrian_controller_old/%.c Drivers/adrian_controller_old/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_PWR_LDO_SUPPLY -DUSE_HAL_DRIVER -DSTM32H753xx -DUSE_NUCLEO_144 -c -I../Core/Inc -I"C:/Users/jells/git/RIT-Baja-eCVT/msd_baja_ecvt_ecu/Drivers/nrf24" -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/BSP/STM32H7xx_Nucleo -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I"../Drivers/adrian_controller" -I../FATFS/Target -I../FATFS/App -I../Middlewares/Third_Party/FatFs/src -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-adrian_controller_old
+
+clean-Drivers-2f-adrian_controller_old:
+	-$(RM) ./Drivers/adrian_controller_old/Controller_P7.cyclo ./Drivers/adrian_controller_old/Controller_P7.d ./Drivers/adrian_controller_old/Controller_P7.o ./Drivers/adrian_controller_old/Controller_P7.su ./Drivers/adrian_controller_old/Controller_P7_data.cyclo ./Drivers/adrian_controller_old/Controller_P7_data.d ./Drivers/adrian_controller_old/Controller_P7_data.o ./Drivers/adrian_controller_old/Controller_P7_data.su ./Drivers/adrian_controller_old/rt_logging.cyclo ./Drivers/adrian_controller_old/rt_logging.d ./Drivers/adrian_controller_old/rt_logging.o ./Drivers/adrian_controller_old/rt_logging.su ./Drivers/adrian_controller_old/rt_nonfinite.cyclo ./Drivers/adrian_controller_old/rt_nonfinite.d ./Drivers/adrian_controller_old/rt_nonfinite.o ./Drivers/adrian_controller_old/rt_nonfinite.su ./Drivers/adrian_controller_old/testmain.cyclo ./Drivers/adrian_controller_old/testmain.d ./Drivers/adrian_controller_old/testmain.o ./Drivers/adrian_controller_old/testmain.su
+
+.PHONY: clean-Drivers-2f-adrian_controller_old
+
